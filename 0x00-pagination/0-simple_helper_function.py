f52f9b192777d48793e2af2ef45399ca6f1573fe
gpradinett <gpradinett@gmail.com>
2022-12-26 20:10:30 -0500

    ✅  update readme

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ Simple helper function
 """
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     """
     Return a tuple of size two containing a start index and an end
     index corresponding to the range of indexes to return in a list
