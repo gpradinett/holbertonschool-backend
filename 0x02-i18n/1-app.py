8406d4a40d77450a00a1653691d397d57e482d1e
gpradinett <gpradinett@gmail.com>
2023-01-03 20:54:00 -0500

    ✅ upload file

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from typing import Any
+from flask_babel import Babel
+from flask import Flask, render_template
+"""
+Basic Babel setup
+"""
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class config language to 'en', 'fr'
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCATE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> Any:
+    """
+    return template of 1-index.html
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
