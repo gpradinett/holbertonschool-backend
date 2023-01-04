0ea90cb5ed1e022f08c177481ccdf83a9db5bd9b
gpradinett <gpradinett@gmail.com>
2023-01-03 19:39:00 -0500

    ✅ upload file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+Basic Flask app
 """
 from flask import Flask, render_template
 from typing import Any
@@ -11,9 +11,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index() -> Any:
     """
-
+    return template of index.html
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
