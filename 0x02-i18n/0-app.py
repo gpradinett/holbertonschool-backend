3855d23271921735fbbaa8f96f31517f592c617d
gpradinett <gpradinett@gmail.com>
2023-01-03 19:35:52 -0500

    ✅ upload file

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from flask import Flask, render_template
+from typing import Any
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> Any:
+    """
+
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
